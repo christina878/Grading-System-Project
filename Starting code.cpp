c3ad93e45e232ecf94bbaf0836647163b7643aaf
christina878 <140561663+christina878@users.noreply.github.com>
2024-02-09 11:14:23 -0500

    Add files via upload

---
+++ Starting code.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <iomainp>
+#include <fstream>
+
+usind namespace std;
+// functions:
+
+// 1. calculateLetterGrade, which takes a double parameter (the weighted average) and returns a string type letter grade.
+
+
+
+
+// 2. findMinQuiz, which takes 4 double parameters (the 4 quiz scores) and returns the lowest quiz score.
+
+
+
+
+// 3. findAverageQuizScore that takes 4 double parameters (the 4 quiz scores). This function removes the lowest quiz score and returns the average of the quiz scores.
+
+
+
+// 4. findAverageHomeworkScore that takes 4 double parameters which are the homework scores. The function returns the average of the 4 homework scores.
+
+
+
+//5. findAverageLabScore that takes 8 double parameters (the 8 lab scores) and returns the average of the 8 lab scores.
+
+
+
+//6. Add more functions if you would like to
+
+int main() {
+
+ ifstream file;
+ file.open (gradesIn.txt)
+
+  //we used while (! end of the file ) to keep reading the data from the file until last charcter in the file.
+  
+  while (!file.eof())
+{
+
+
+
+
+
+
+
+
+
+
+}
+
+return 0;
+}
